4a82e2fd6d652a5c3157bfa4694d6e32437e4aa5
zhedezhao <zhaozhede@gmail.com>
2022-09-06 08:10:09 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+module.exports = {
+  //关闭eslint
+  lintOnSave:false
+}
